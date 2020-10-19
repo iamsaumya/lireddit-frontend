85be51d05aa8102419284797fc02266d231d6e37
Saumya Pandey <sp160899@gmail.com>
2020-10-20 01:27:23 +0530

    Create Registration form

---
+++ src/components/InputField.tsx
@@ -0,0 +1,28 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage
+} from '@chakra-ui/core';
+import { useField } from 'formik';
+import React, { InputHTMLAttributes } from 'react';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size,
+  ...props
+}) => {
+  const [field, { error }] = useField(props); // explained in https://formik.org/docs/api/useField
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
+};
---
+++ src/pages/register.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+import { Box, Button } from '@chakra-ui/core';
+
+interface registerProps {}
+
+const register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{
+          username: '',
+          password: ''
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          //custom input field
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              mt={4}
+              backgroundColor="blue.300"
+              isLoading={isSubmitting}
+              color="white"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+// in nextjs we have to export the default component
+export default register;
